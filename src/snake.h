3c20511bcdecdfb635769049eaeca0f2804d88d8
aakarshan-raj <aakarshan7raj@gmail.com>
2023-05-17 15:01:10 +0530

    check

--- src/snake.h
+++ src/snake.h
@@ -1,6 +1,7 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 #include "SDL2/SDL.h"
+#include <vector>
 
 class Snake{
     public:
@@ -10,12 +11,20 @@ class Snake{
           head_x(grid_width/1),
           head_y(grid_height/2){}
     Direction direction = Direction::kUp;      
-int size{0};
-   private:
-   float head_x;
-   float head_y;
-   int grid_width;
-   int grid_height;
+
+    int size{0};
+    bool alive{true};
+    void Update();
+    void UpdateHead();
+    void UpdateBody(SDL_Point& , SDL_Point&);
+    float speed{0.1};
+    std::vector<SDL_Point> body;
+    bool growing{false};
+    private:
+    float head_x;
+    float head_y;
+    int grid_width;
+    int grid_height;
    
 
 
